eb1e7834c017e016251eb5d1797ac6cc01cc38a3
Florent Brosse <florent.brosse@datastax.com>
2019-01-07 17:23:17 +0100

    limit log, change dissect to cassandra, improve filebeat fields

---
+++ elk/filebeat/filebeat_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+apt update
+apt install curl -y
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.5.4-amd64.deb
+dpkg -i filebeat-6.5.4-amd64.deb
+
+cat > /etc/filebeat/filebeat.yml <<EOF
+output.elasticsearch:
+    enabled: true
+    hosts: ["elasticsearch:9200"]
+setup.kibana:
+  host: "kibana:5601"
+setup.dashboards.enabled: true
+filebeat.inputs:
+    - type: log
+      paths:
+        - "/var/log/cassandra/system.log*"
+      document_type: cassandra_system_logs
+      exclude_files: ['\.zip$']
+      multiline.pattern: '^TRACE|DEBUG|WARN|INFO|ERROR'
+      multiline.negate: true
+      multiline.match: after
+processors:
+    - dissect:
+            target_prefix: "cassandra" 
+            tokenizer: "%{level} [%{thread}] %{date} %{time} %{service} %{class}:%{line} - %{msg}"
+EOF
+
+service filebeat start
